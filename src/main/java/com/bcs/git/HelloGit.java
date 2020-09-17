5df397eec9d8afbf25961df4a988624037e523e5
luffy953 <823539720@qq.com>
2020-09-17 23:07:24 +0800

    分支解决冲突

--- src/main/java/com/bcs/git/HelloGit.java
+++ src/main/java/com/bcs/git/HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 		System.out.println("hello git");
 		System.out.println("更新1");
 		System.out.println("创建分支");
+		System.out.println("解决冲突");
+		System.out.println("解决");
+		System.out.println("分支添加");
 	}
 
 }
